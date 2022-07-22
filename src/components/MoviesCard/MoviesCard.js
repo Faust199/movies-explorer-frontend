3773d316a3194a0a70e17d80abea2b18e7e24f15
Nikita Polezhaev <polnikita199@yandex.ru>
2022-07-22 10:19:12 +0300

    duration on card fixed

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -13,21 +13,21 @@ function MoviesCard(props) {
                 <button onClick={handleButtonClick}  className={'moviescard__button moviescard__button_deleted'}/>
             );
         }
-        // else {
-        //     return (
-        //         <button onClick={handleButtonClick}  className={'moviescard__button moviescard__button_selected'} />
-        //     );
-        // }
     }
 
     const handleButtonClick = (e) => {
          if (props.from === 'SavedMovies') {
              props.onDeleteMovie(props.item);
         } else {
-             // let isSelected = e.target.classList.contains('moviescard__button_selected');
-             // const action = isSelected ? "remove" : "add";
-             // e.target.classList[action]('moviescard__button_selected');
              props.onSaveMovie(props.item, e)
+        }
+    }
+
+    const setCorrectHours = () => {
+        if (Math.floor(props.item.duration / 60) > 1) {
+            return `${Math.floor(props.item.duration / 60)} часа`
+        } else {
+            return `${Math.floor(props.item.duration / 60)} час`
         }
     }
 
@@ -35,7 +35,7 @@ function MoviesCard(props) {
         <div className={'moviescard'}>
             <div className={'moviescard__container'}>
                 <p className={'moviescard__title'}>{props.item.nameRU}</p>
-                <p className={'moviescard__time'}>{`${props.item.duration} минут`}</p>
+                <p className={'moviescard__time'}>{`${setCorrectHours()} ${props.item.duration % 60} мин`}</p>
             </div>
             <img src={props.from === 'Movies' ? `https://api.nomoreparties.co${props.item.image.url}` : props.item.image} className={'moviescard__image'} alt={'Картинка'}/>
             {createButton()}
