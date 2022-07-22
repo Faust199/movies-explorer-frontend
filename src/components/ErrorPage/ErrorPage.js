3318a71ce6006ebeab5b865e98cdffb36709c29c
Nikita Polezhaev <polnikita199@yandex.ru>
2022-07-22 12:53:39 +0300

    404 page added

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,10 +1,16 @@
 import './ErrorPage.css'
 import React from 'react';
+import {useHistory} from "react-router-dom";
 
 function ErrorPage(props) {
+
+    const history = useHistory();
+
     return(
-        <div>
-           <h4>HELLO ERROR</h4>
+        <div className={'error_page'}>
+            <h4 className={'error_title'}>404</h4>
+            <p className={'error_subtitle'}>Страница не найдена</p>
+            <button className={'error_button'} onClick={history.goBack}>Назад</button>
         </div>
     );
 }
